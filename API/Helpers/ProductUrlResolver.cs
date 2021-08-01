d754273195a59af9b0919eb8ed9b3b64195d44bf
hari8592 <harishpawar92@gmail.com>
2021-08-01 20:08:09 +0530

    End Of Section 4

---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using API.DTOs;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDTO, string>
+    {
+        //use Microsoft.Configuration imp note down
+        private readonly IConfiguration _config;
+        public ProductUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string Resolve(Product source, ProductToReturnDTO destination, 
+                               string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _config["ApiUrl"] + source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}
---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public interface ISpecification<T>
+    {
+        //get product type id =1 i.e criteria means here .Where
+        Expression<Func<T,bool>> Criteria { get;  }
+        List<Expression<Func<T,object>>> Includes { get; }
+    }
+}
