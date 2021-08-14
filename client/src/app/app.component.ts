7cfdd64095377c08bdb5d000d3625b05dc47f855
hari8592 <harishpawar92@gmail.com>
2021-08-14 17:42:57 +0530

    End Of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  
+  title = 'Book Shop';
+  products: IProduct  [] = [];
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get<IPagination>('https://localhost:44378/api/products').subscribe(
+      (response: IPagination) => {
+      this.products=response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
