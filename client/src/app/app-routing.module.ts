834108cee3fc52a058b12d0b95fc01a6a2aa9778
hari8592 <harishpawar92@gmail.com>
2021-08-27 11:29:47 +0530

    End Of Section 11

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'shop',loadChildren:() => 
-                import ('./shop/shop-routing.module').then(mod=>mod.ShopRoutingModule)},
-  
-  {path:'**',redirectTo:'',pathMatch:'full'}
+  { path: '', component: HomeComponent },
+  { path: 'test-error', component: TestErrorComponent },
+  {
+    path: 'shop', loadChildren: () =>
+      import('./shop/shop-routing.module').then(mod => mod.ShopRoutingModule)
+  },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
---
+++ client/src/app/core/interceptor/error.interceptor.ts
@@ -0,0 +1,45 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { NavigationExtras, Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(private rotuer: Router, private toastr: ToastrService) {
+    }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError(error => {
+                if (error) {
+
+                    if (error.status === 400) {
+                        if(error.error.errors){
+                            throw error.error.errors;
+                        }
+                        else{
+                        this.toastr.error(error.error.message, error.error.statusCode);
+                        }
+                    }
+
+                    if (error.status === 401) {
+                        this.toastr.error(error.error.message, error.error.statusCode);
+                    }
+
+                    if (error.status === 404) {
+                        this.rotuer.navigateByUrl('/not-found');
+                    }
+
+                    if (error.status === 500) {
+                        const navigationExtras:NavigationExtras={state:{error:error.error}}
+                        this.rotuer.navigateByUrl('/server-error',navigationExtras);
+                    }
+                }
+                return throwError(error);
+            })
+        )
+    }
+
+}
